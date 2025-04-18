8a0dbc1caf5f0216fbafdd3a7de4507b88f34100
YawHB <yawhboateng@gmail.com>
2025-04-18 11:20:50 +0200

    create custom data scala. logic not complete, but now Date type can be fetched

--- scalas/date.js
+++ scalas/date.js
@@ -1,15 +1,17 @@
 import { GraphQLScalarType, Kind } from 'graphql'
 
-const dateScala = new GraphQLScalarType({
+export const dateScala = new GraphQLScalarType({
   name: 'Date',
   description: 'Date custom scala type',
 
   //Sender værdier til klienten
   serialize(value) {
+    return value.toString()
+    //"2025-12-02"
     // if (value instanceof Date) {
     //   return value.getTime()
     // }¨
-    return dateToString(value)
+    // return dateToString(value)
   },
   //Modtager en værdi fra klienten
   parseValue(value) {
