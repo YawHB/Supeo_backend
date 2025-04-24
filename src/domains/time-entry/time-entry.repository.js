80735de6ae27f86922d8e8373b4bac336223871b
YawHB <yawhboateng@gmail.com>
2025-04-24 22:52:27 +0200

    add repository sql calls to create a timeEntry with notification dependency

--- src/domains/time-entry/time-entry.repository.js
+++ src/domains/time-entry/time-entry.repository.js
@@ -1,9 +1,32 @@
 export async function createTimeEntry(sql, newTimeEntry) {
-  const { startTime, endTime, duration, comment, date, employeeID } = newTimeEntry
-  const result =
-    await sql`INSERT INTO time_entry ("start_time", "end_time", "duration", "comment", "date","employee_id")
-    VALUES(${startTime}, ${endTime},${duration},${comment},${date},${employeeID})
-    RETURNING *
-      `
-  return result[0]
+  const { startTime, endTime, duration, comment, date, employeeID, notification } = newTimeEntry
+  const { comment: notificationComment, timestamp, status } = notification
+  console.log('-----------------------------')
+  console.log('newTimeEntry in repository: ', newTimeEntry)
+
+  const notificationResultArr =
+    await sql`INSERT INTO notification ("comment", "timestamp", "status")
+  VALUES(${notificationComment}, ${timestamp}, ${status})
+  RETURNING *
+  `
+  const notificationID = notificationResultArr[0].id
+  const notificationResult = notificationResultArr[0]
+
+  //   console.log('notificationResult: ', notificationResult[0])
+  //   console.log('notificationID: ', notificationID)
+  console.log('-----------------------------')
+  const timeEntryResultArr =
+    await sql`INSERT INTO time_entry ("start_time", "end_time", "duration", "comment", "date","employee_id", "notification_id")
+  VALUES(${startTime}, ${endTime},${duration},${comment},${date},${employeeID}, ${notificationID})
+  RETURNING *
+  `
+
+  console.log('timeEntryResultArr: ', timeEntryResultArr)
+  console.log('-----------------------------')
+
+  return {
+    ...timeEntryResultArr[0],
+    notification: notificationResult,
+  }
+  return timeEntryResultArr[0]
 }
