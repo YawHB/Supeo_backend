4b1ce04bbd6cadc0b56b86c60165d084597749b6
YawHB <yawhboateng@gmail.com>
2025-04-24 22:58:23 +0200

    add timestamp resolver

--- resolvers/dateResolver.js
+++ resolvers/dateResolver.js
@@ -1,4 +1,6 @@
 import { dateScala } from '../src/scalas/Date.scalar.js'
+import { TimestampType } from '../src/scalas/TimeStamp.scalar.js'
 export const scalaResolvers = {
   Date: dateScala,
+  MyTimeStamp: TimestampType,
 }
