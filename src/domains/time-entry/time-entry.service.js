c8f7d009ce667e8295f33f8d2296af9df4326513
YawHB <yawhboateng@gmail.com>
2025-04-28 17:53:44 +0200

    create string time to timestamp convertion functions

--- src/domains/time-entry/time-entry.service.js
+++ src/domains/time-entry/time-entry.service.js
@@ -1,3 +1,4 @@
+import { networkInterfaces } from 'os'
 import {
   findAllTimeEntries,
   findTimeEntryById,
@@ -31,5 +32,38 @@ export async function updateStatus(id, status, sql) {
 }
 
 export async function addNewTimeEntry(sql, newTimeEntry) {
-  return await createTimeEntry(sql, newTimeEntry)
+  console.log('Inside add new time entry -- service')
+  console.log(newTimeEntry)
+  const { date, startTime, endTime, notification } = newTimeEntry
+  const { timestamp } = notification
+  console.log('notification: ', timestamp)
+  const convertedTimeEntry = {
+    ...newTimeEntry,
+    startTime: unixTimeToTimestamp(combineDateAndTimeToUnixTime(date, startTime)),
+    endTime: unixTimeToTimestamp(combineDateAndTimeToUnixTime(date, endTime)),
+    notification: {
+      ...notification,
+      timestamp: unixTimeToTimestamp(timestamp),
+    },
+  }
+  console.log('-----------------hallloo-----------------------')
+  console.log('convertedTimeEntry: ', convertedTimeEntry)
+  return await createTimeEntry(sql, convertedTimeEntry)
+}
+
+function combineDateAndTimeToUnixTime(currentDate, time) {
+  const tempDate = currentDate
+  const stringDate = tempDate.toISOString()
+  const [date] = stringDate.split('T')
+  const dateToParse = `${date} ${time}`
+  const unixTimestamp = Date.parse(dateToParse)
+
+  return unixTimestamp
+  //---------------
+}
+function unixTimeToTimestamp(unixTime) {
+  unixTime = Number(unixTime)
+  const timestamp = new Date(unixTime).toISOString()
+  console.log('timestamp: ', timestamp)
+  return timestamp
 }
