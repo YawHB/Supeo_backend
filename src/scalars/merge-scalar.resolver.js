6273eeb2fae21d5bae26fe55b83335d3e7e480ab
YawHB <yawhboateng@gmail.com>
2025-04-29 23:02:27 +0200

    merge custom Scalar

--- src/scalars/merge-scalar.resolver.js
+++ src/scalars/merge-scalar.resolver.js
@@ -1,7 +1,9 @@
 import { dateScalar } from './date-scalar.js'
 import { timestampScalar } from './timestamp-scalar.js'
+import { unixScalar } from './unix-scalar.js'
 
 export const scalaResolver = {
   Date: dateScalar,
   Time: timestampScalar,
+  Unix: unixScalar,
 }
