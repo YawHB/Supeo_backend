abbbfe17baa8aceb84ef2bb7fe450b44a560e28f
Nikolaj Christian Møller <nikolajchristianmoeller@gmail.com>
2025-05-07 08:28:42 +0200

    Update employee.repository.js

--- src/domains/employee/employee.repository.js
+++ src/domains/employee/employee.repository.js
@@ -16,40 +16,40 @@ export async function getEmployeeById(sql, id) {
 }
 
 export async function createEmployee(sql, employee) {
-  const { firstName, lastName, email, role, phoneNumber } = employee
+  const { firstName, lastName, email, phoneNumber } = employee
   const result = await sql`
-    INSERT INTO employee ("first_name", "last_name", "email", "role", "phone_number")
-    VALUES (${firstName}, ${lastName}, ${email}, ${role}, ${phoneNumber})
+    INSERT INTO employee ("first_name", "last_name", "email", "phone_number")
+    VALUES (${firstName}, ${lastName}, ${email}, ${phoneNumber})
     RETURNING *
   `
   return result[0]
 }
 
 export async function updateEmployee(sql, id, employee) {
-  const { firstName, lastName, email, role, phoneNumber } = employee
+  const { firstName, lastName, email, phoneNumber } = employee
   const result = await sql`
     UPDATE employee
-    SET "first_name" = ${firstName}, "last_name" = ${lastName}, "email" = ${email}, "role" = ${role}, "phone_number" = ${phoneNumber}
+    SET "first_name" = ${firstName}, "last_name" = ${lastName}, "email" = ${email},  "phone_number" = ${phoneNumber}
     WHERE id = ${id}
     RETURNING *
   `
   return result[0]
 }
 
-export async function countEmployees(sql) {
-  const [{ count }] = await sql`
-    SELECT COUNT(*)::int AS count
-    FROM employee
-  `
-  return count
-}
+// export async function countEmployees(sql) {
+//   const [{ count }] = await sql`
+//     SELECT COUNT(*)::int AS count
+//     FROM employee
+//   `
+//   return count
+// }
 
-export async function getEmployeesPaginated(sql, { page, perPage }) {
-  const offset = (page - 1) * perPage
-  return await sql`
-    SELECT *
-    FROM employee
-    LIMIT ${perPage}
-    OFFSET ${offset}
-  `
-}
+// export async function getEmployeesPaginated(sql, { page, perPage }) {
+//   const offset = (page - 1) * perPage
+//   return await sql`
+//     SELECT *
+//     FROM employee
+//     LIMIT ${perPage}
+//     OFFSET ${offset}
+//   `
+// }
