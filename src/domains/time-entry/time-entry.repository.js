0503c196f043620ab61af3d3866d960593a26283
YawHB <yawhboateng@gmail.com>
2025-05-15 16:28:20 +0200

    add default break to create time entry

--- src/domains/time-entry/time-entry.repository.js
+++ src/domains/time-entry/time-entry.repository.js
@@ -27,8 +27,8 @@ export async function createTimeEntry(sql, newTimeEntry) {
   const notificationResult = notificationResultArr[0]
 
   const timeEntryResultArr =
-    await sql`INSERT INTO time_entry ("start_time", "end_time", "duration", "comment", "start_date", "end_date", "employee_id", "notification_id")
-  VALUES(${startTime}, ${endTime}, ${duration}, ${comment}, ${startDate}, ${endDate}, ${employeeID}, ${notificationID})
+    await sql`INSERT INTO time_entry ("start_time", "end_time", "duration", "break", "comment", "start_date", "end_date", "employee_id", "notification_id")
+  VALUES(${startTime}, ${endTime}, ${duration}, ${DEFAULT_BREAK}, ${comment}, ${startDate}, ${endDate}, ${employeeID}, ${notificationID})
   RETURNING *
   `
 
