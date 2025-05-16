eb1210176d7e81c9181d0661f53bb81fb622b021
Nikolaj Christian Møller <nikolajchristianmoeller@gmail.com>
2025-05-16 16:32:40 +0200

    move user menu resolver into utils folder + corrected path from merge resolvers

--- src/schema-builder/merge-resolvers.js
+++ src/schema-builder/merge-resolvers.js
@@ -2,7 +2,7 @@ import { employeeResolver } from '../domains/employee/employee.controller.js'
 import { timeEntryResolver } from '../domains/time-entry/time-entry.controller.js'
 import { scalaResolver } from '../scalars/merge-scalar.resolver.js'
 import { mergeResolvers } from '@graphql-tools/merge'
-import { userMenuResolver } from '../services/user-menu.resolver.js'
+import { userMenuResolver } from '../utils/user-menu.resolver.js'
 import { notificationResolver } from '../domains/notification/notification.controller.js'
 
 export const resolvers = mergeResolvers([
