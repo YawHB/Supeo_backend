478c505e9a7db92037169d18ded317ca6eabb7c7
Nikolaj Christian Møller <nikolajchristianmoeller@gmail.com>
2025-05-16 16:27:25 +0200

    remove admin resolver from merge resolvers

--- src/schema-builder/merge-resolvers.js
+++ src/schema-builder/merge-resolvers.js
@@ -1,6 +1,5 @@
 import { employeeResolver } from '../domains/employee/employee.controller.js'
 import { timeEntryResolver } from '../domains/time-entry/time-entry.controller.js'
-import { adminResolver } from '../domains/admin/admin.controller.js'
 import { scalaResolver } from '../scalars/merge-scalar.resolver.js'
 import { mergeResolvers } from '@graphql-tools/merge'
 import { userMenuResolver } from '../services/user-menu.resolver.js'
@@ -8,7 +7,6 @@ import { notificationResolver } from '../domains/notification/notification.contr
 
 export const resolvers = mergeResolvers([
   employeeResolver,
-  adminResolver,
   timeEntryResolver,
   scalaResolver,
   userMenuResolver,
