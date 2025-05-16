d07c0df651802f449f4055f353ccfcfaf873b7bb
Nikolaj Christian Møller <nikolajchristianmoeller@gmail.com>
2025-05-16 18:11:43 +0200

    add line and order imports

--- src/schema-builder/merge-type-defs.js
+++ src/schema-builder/merge-type-defs.js
@@ -1,6 +1,6 @@
+import { printSchema } from 'graphql'
 import { loadSchemaSync } from '@graphql-tools/load'
 import { GraphQLFileLoader } from '@graphql-tools/graphql-file-loader'
-import { printSchema } from 'graphql'
 
 export const typeDefs = loadSchemaSync('./**/*.graphql', {
   loaders: [new GraphQLFileLoader()],
