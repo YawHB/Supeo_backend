021cd1fe23a3d3ba77c6a4d80d5797225e0794d4
Nikolaj Christian Møller <nikolajchristianmoeller@gmail.com>
2025-05-19 17:09:24 +0200

    add to the service

--- src/domains/time-entry/time-entry.service.js
+++ src/domains/time-entry/time-entry.service.js
@@ -6,6 +6,7 @@ import {
   findAllTimeEntries,
   updateTimeEntryStatus,
   findOverlappingTimeEntries,
+  
 } from './time-entry.repository.js'
 
 export async function fetchTimeEntryById(id, sql) {
@@ -32,4 +33,22 @@ export async function addNewTimeEntry(sql, newTimeEntry) {
   if (!datesInOrder || !timesInOrder) throw new WorkHoursAreNegative()
 
   return await createTimeEntry(sql, newTimeEntry)
+}
+
+export async function removeTimeEntry(id, sql) {
+  const deletedTimeEntry = await deleteTimeEntryById(id, sql)
+  return deletedTimeEntry?.id || null
+}
+
+export async function updateTimeEntry(sql, id, updatedTimeEntry) {
+  const { employeeID, startTime, endTime, startDate, endDate } = updatedTimeEntry
+  const datesInOrder = hasCorrectOrder(startDate, endDate)
+  const timesInOrder = hasCorrectOrder(startTime, endTime)
+
+  const overlappingTimeEntries = await findOverlappingTimeEntries(employeeID, startTime, endTime)
+  const overlapsExcludingSelf = overlappingTimeEntries.filter((entry) => entry.id !== id)
+  if (overlapsExcludingSelf.length > 0) throw new OverlappingTimeEntryExist()
+  if (!datesInOrder || !timesInOrder) throw new WorkHoursAreNegative()
+
+  return await updateTimeEntryAndResetStatus(sql, id, updatedTimeEntry)
 }
